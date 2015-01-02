a7faa2390abe8c5d31e39cc53ce8b04a6f213695
Michael Hathaway <michael@hathaways.me.uk>
2015-01-02 13:27:26 +0000

    Actually use if/else properly

--- java-socketchat-client/src/backend/client.java
+++ java-socketchat-client/src/backend/client.java
@@ -28,28 +28,26 @@ public class client implements Runnable{
 	public void run() {
 		try {
 			if(Layout.getPreviousMessage().equals(message)){
-				this.finalize();
-			}
-	        InetAddress addr = InetAddress.getByName(recipient);
-	        Socket theSocket = new Socket(addr, 49149);
-	        PrintWriter out = new PrintWriter(theSocket.getOutputStream(), true);
-	        System.out.println(
-	        		"Connected to "+ theSocket.getInetAddress()+ 
-	        		" on port " + theSocket.getPort() + 
-	        		" from port "+ theSocket.getLocalPort() + 
-	        		" of "+ theSocket.getLocalAddress());
-	        out.println(message);
-	        theSocket.close();
-	     } 
-	     catch (UnknownHostException e) {
-	        System.err.println("I can't find " + e  );
-	     }
-	     catch (SocketException e) {
-	        System.err.println("Could not connect to " +e );
-	     }
-	     catch (IOException e) {
-	        System.err.println(e);
-	     } catch (Throwable e) {
+				System.out.println("Not sending- previous message is this message.");
+			}else{
+		        InetAddress addr = InetAddress.getByName(recipient);
+		        Socket theSocket = new Socket(addr, 49149);
+		        PrintWriter out = new PrintWriter(theSocket.getOutputStream(), true);
+		        System.out.println(
+		        		"Connected to "+ theSocket.getInetAddress()+ 
+		        		" on port " + theSocket.getPort() + 
+		        		" from port "+ theSocket.getLocalPort() + 
+		        		" of "+ theSocket.getLocalAddress());
+		        out.println(message);
+		        theSocket.close();
+		     } 
+		}catch (UnknownHostException e) {
+			System.err.println("I can't find " + e  );
+		}catch (SocketException e) {
+			System.err.println("Could not connect to " +e );
+		}catch (IOException e) {
+		   System.err.println(e);
+		} catch (Throwable e) {
 			e.printStackTrace();
 		}
 	}
